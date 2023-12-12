0dcee4677fb0c0eaa1c219f3a0b2d7363c8390bf
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-12 18:17:43 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0 (#47)

--- awscfn/connection_config.go
+++ awscfn/connection_config.go
@@ -2,18 +2,10 @@ package awscfn
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type awscfnConfig struct {
-	Paths []string `cty:"paths" steampipe:"watch"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"paths": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
+	Paths []string `hcl:"paths" steampipe:"watch"`
 }
 
 func ConfigInstance() interface{} {
